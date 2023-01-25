de5853b9ab2b9b901ad04de7f2520d11d31aaad6
joaquinns <joaquindevv96@gmail.com>
2023-01-25 12:06:56 -0400

    added styles to div

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,14 @@
+import { Header } from '../Header'
+
 interface LayoutProps {
   children: JSX.Element
 }
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
-      <main className='min-h-screen dark:bg-zinc-900 text-slate-200'>
-        {children}
-      </main>
-      <footer>zoi un footer :D</footer>
-    </>
+    <div className='min-h-screen dark:bg-zinc-900 dark:bg-gradient-to-l dark:from-transparent bg-gradient-to-l from-slate-100 dark:text-slate-200 transition-colors duration-200'>
+      <Header />
+      <main className='min-h-screen'>{children}</main>
+    </div>
   )
 }
